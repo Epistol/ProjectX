7401d1f1138f19ae01e739ef5a6c6702ea9e23fc
Epistol <mika63270@live.fr>
2016-02-06 20:02:15 +0100

    yo

--- main.c
+++ main.c
@@ -34,7 +34,7 @@ void dialoguesalle(int SalleEnCours, char Etat_Joueur) {
                 Salle[3] = 0;
 
 
-
+//PLOP2
 
                     printf("|-----------| \n");
                     printf("|          /| \n");
